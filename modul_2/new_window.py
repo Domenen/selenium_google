6adb25c3856c4a7491d223bf67d13f4dd042dc53
Domenen <Diman.9533@gmail.com>
2025-02-21 13:28:23 +0300

    'new_window'

---
+++ modul_2/new_window.py
@@ -0,0 +1,36 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try: 
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/redirect_accept.html")
+
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+
+    text_calc = browser.find_element(By.ID, "input_value")
+    y = calc(text_calc.text)
+
+    input_1 = browser.find_element(By.ID, "answer")
+    input_1.send_keys(f"{y}")
+
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
+
+    print(browser.switch_to.alert.text.split()[-1])
+
+
+finally:
+    time.sleep(5)
+    browser.quit()
