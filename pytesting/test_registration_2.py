22d36a2e895fb9621c5fdb86ef87267acf194be8
Domenen <Diman.9533@gmail.com>
2025-02-25 17:24:11 +0300

    need_fix_registrations_2

--- pytesting/test_registration_2.py
+++ pytesting/test_registration_2.py
@@ -15,16 +15,23 @@ load_dotenv()
 LOGIN = os.getenv("LOGIN") or "login"
 PASSWORD = os.getenv("PASSWORD") or "password"
 
+# links = ["https://stepik.org/lesson/236896/step/1"]
+links = ["https://stepik.org/lesson/236895/step/1",
+         "https://stepik.org/lesson/236896/step/1", 
+         "https://stepik.org/lesson/236897/step/1", 
+         "https://stepik.org/lesson/236898/step/1", 
+         "https://stepik.org/lesson/236899/step/1",
+         "https://stepik.org/lesson/236903/step/1", 
+         "https://stepik.org/lesson/236904/step/1",
+         "https://stepik.org/lesson/236905/step/1"]
 
-links = ["https://stepik.org/lesson/236895/step/1"]
-        #  "https://stepik.org/lesson/236896/step/1", 
-        #  "https://stepik.org/lesson/236897/step/1", 
-        #  "https://stepik.org/lesson/236898/step/1", 
-        #  "https://stepik.org/lesson/236899/step/1",
-        #  "https://stepik.org/lesson/236903/step/1", 
-        #  "https://stepik.org/lesson/236904/step/1",
-        #  "https://stepik.org/lesson/236905/step/1"]
-
+@pytest.fixture(scope="session")
+def collected_results():
+    results = []
+    yield results
+    print("\nРезультаты всех тестов:")
+    for result in results:
+        print(result)
 
 @pytest.fixture(scope="function")
 def browser():
@@ -36,15 +43,16 @@ def browser():
 
 @pytest.mark.parametrize('site_link', links)
 def test_guest_should_see_login_link(browser, site_link):
+
     link = f"{site_link}"
     browser.get(link)
 
-    button_auth = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable(By.CLASS_NAME, "navbar__auth"))
+    button_auth = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "navbar__auth")))
     button_auth.click()
 
-    input_name = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located(By.CSS_SELECTOR, "input[name='login']"))
+    input_name = WebDriverWait(browser, 15).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='login']")))
     input_name.send_keys(f"{LOGIN}")
 
     input_pass = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
@@ -52,17 +60,30 @@ def test_guest_should_see_login_link(browser, site_link):
 
     button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
     button.click()
+    
+    textarea_free = browser.find_element(
+        By.CLASS_NAME, "quiz-component"
+    ).get_attribute("data-state")
+    if textarea_free != "correct":
+        input_textarea = browser.find_element(By.CLASS_NAME, "string-quiz__textarea")
 
-    input_textarea = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable(By.CLASS_NAME,
-                                              "string-quiz__textarea"))
-    answer = math.log(int(time.time()))
-    input_textarea.send_keys(f"{answer}")
+        answer = math.log(int(time.time()))
+        input_textarea.send_keys(answer)
+        time.sleep(3)
+        input_textarea = browser.find_element(By.CLASS_NAME, "string-quiz__textarea")
+        answer = math.log(int(time.time()))
+        input_textarea.send_keys(answer)
+        button_smb = browser.find_element(
+            By.CSS_SELECTOR, "button.submit-submission"
+        )
+        button_smb.click()
+        # print("После нажатия")
+        # time.sleep(5)
 
-    button_send = browser.find_element(By.CLASS_NAME, "submit-submission")
-    button_send.click()
-
-    answer_text = WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located(By.CLASS_NAME, "attempt-message")
-    )
-    print(answer_text.text == "Correct!")
+    answer_text = WebDriverWait(browser, 15).until(
+			EC.visibility_of_element_located(
+				(By.CLASS_NAME, "smart-hints__hint"))
+		)
+    if answer_text.text != "Correct!":
+        new_text = answer_text.text
+        collected_results.append(new_text)
